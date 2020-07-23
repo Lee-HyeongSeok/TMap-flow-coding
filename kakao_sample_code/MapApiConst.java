1008d0456e3731c89a109a86a1e7423109812a08
leehyeongseck <kea7109@naver.com>
2020-07-23 17:56:51 +0900

    kakao sample code with kakao map api

---
+++ kakao_sample_code/MapApiConst.java
@@ -0,0 +1,9 @@
+package net.daum.android.map.openapi.sampleapp;
+
+/**
+ * Created by pjh on 2014. 9. 15..
+ */
+public class MapApiConst {
+	// http://developers.daum.net/console
+    public static final String DAUM_MAPS_ANDROID_APP_API_KEY = "6f504f9b73ad280372b2aff0036b6f32";
+}
