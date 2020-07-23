f3aae37b3fe53bed28250b1e54068be1f883c835
leehyeongseck <55940552+leehyeongseck@users.noreply.github.com>
2020-07-23 18:09:03 +0900

    Update MapApiConst.java

--- kakao_sample_code/MapApiConst.java
+++ kakao_sample_code/MapApiConst.java
@@ -5,5 +5,5 @@ package net.daum.android.map.openapi.sampleapp;
  */
 public class MapApiConst {
 	// http://developers.daum.net/console
-    public static final String DAUM_MAPS_ANDROID_APP_API_KEY = "6f504f9b73ad280372b2aff0036b6f32";
+    public static final String DAUM_MAPS_ANDROID_APP_API_KEY = "Your Native App key";
 }
