8dfebc2e61bb455877a444fccebb20794e639981
jade1224 <alley122024@naver.com>
2020-07-27 21:22:49 +0900

    firebase realtime DB를 이용해 마커 찍기

---
+++ kakaoMap/app/src/main/java/kr/psk/kpu/kakaomap/Kicks.kt
@@ -0,0 +1,13 @@
+package kr.psk.kpu.kakaomap
+
+public class Kicks {
+    var latitude:Double
+    var longitude:Double
+    var kicksnum:Int
+
+    constructor(latitude:Double,longitude:Double,kicksnum:Int){
+        this.latitude=latitude
+        this.longitude=longitude
+        this.kicksnum=kicksnum
+    }
+}
